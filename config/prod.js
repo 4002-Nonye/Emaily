676c86df38c8e2e4de5577d56af4adcd58571af4
4002-Nonye <royaltysungbin@gmail.com>
2025-02-16 20:58:55 +0100

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -5,4 +5,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
-// 'mongodb+srv://ojukwuchinonye70:ZvL8eeaFdrQ4c1UA@cluster0.fg4i1.mongodb.net/emailyprod?retryWrites=true&w=majority&appName=Cluster0'
